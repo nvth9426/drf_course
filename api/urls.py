e1be70a2fbf65abc097c4fce3e29727d0d307ccd
Winston Trinh <nvth9426@yahoo.com>
2025-11-30 09:44:12 -0500

    ListCreateAPIView & RetrieveAPIView

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 
 urlpatterns = [
-  path('products/', views.product_list, name='products'),
-  path('products/<int:pk>/', views.product_detail, name='product_detail'),
+  # path('products/', views.product_list, name='products'),
+  # path('products/<int:pk>/', views.product_detail, name='product_detail'),
+  path('products/', views.ProductListAPIView.as_view()),
+  path('products/<int:product_id>/', views.ProductDetailAPIView.as_view()),
   path('products/info/', views.product_info, name='product_info'),
-  path('orders/', views.order_list, name='orders'),
+  # path('orders/', views.order_list, name='orders'),
+  path('orders/', views.OrdertListAPIView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -5,26 +5,46 @@ from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSeria
 from api.models import Product, Order
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
-@api_view(['GET'])
-def product_list(request):
-  products = Product.objects.all()
-  serializer = ProductSerializer(products, many=True)
+class ProductListAPIView(generics.ListCreateAPIView):
+  # queryset = Product.objects.all()
   
-  # return JsonResponse({'data': serializer.data})
-  return Response(serializer.data)
+  # return products in stock
+  # queryset = Product.objects.filter(stock__gt=0)
+  
+  # return products out of stock
+  # queryset = Product.objects.exclude(stock__gt=0)
+  serializer_class = ProductSerializer
 
-@api_view(['GET'])
-def product_detail(request, pk):
-  product = get_object_or_404(Product, pk=pk)
-  serializer = ProductSerializer(product)
-  return Response(serializer.data)
+class ProductDetailAPIView(generics.RetrieveAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+  lookup_url_kwarg = 'product_id'
 
-@api_view(['GET'])
-def order_list(request):
-  orders = Order.objects.prefetch_related('items__product')
-  serializer = OrderSerializer(orders, many=True)
-  return Response(serializer.data)
+class OrdertListAPIView(generics.ListCreateAPIView):
+  queryset = Order.objects.prefetch_related('items__product')
+  serializer_class = OrderSerializer
+  
+# @api_view(['GET'])
+# def product_list(request):
+#   products = Product.objects.all()
+#   serializer = ProductSerializer(products, many=True)
+  
+#   # return JsonResponse({'data': serializer.data})
+#   return Response(serializer.data)
+
+# @api_view(['GET'])
+# def product_detail(request, pk):
+#   product = get_object_or_404(Product, pk=pk)
+#   serializer = ProductSerializer(product)
+#   return Response(serializer.data)
+
+# @api_view(['GET'])
+# def order_list(request):
+#   orders = Order.objects.prefetch_related('items__product')
+#   serializer = OrderSerializer(orders, many=True)
+#   return Response(serializer.data)
 
 @api_view(['GET'])
 def product_info(request):
