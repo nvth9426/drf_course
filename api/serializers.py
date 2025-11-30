45bd6b98845765446b7f0f6d109780b1bf511a31
Winston Trinh <nvth9426@yahoo.com>
2025-11-30 07:56:47 -0500

    Serializer subclasses & Aggregate API data

--- api/serializers.py
+++ api/serializers.py
@@ -53,3 +53,8 @@ class OrderSerializer(serializers.ModelSerializer):
       'total_price'
     )
 
+class ProductInfoSerializer(serializers.Serializer):
+  products = ProductSerializer(many=True)
+  count = serializers.IntegerField()
+  max_price = serializers.FloatField()  
+
--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
   path('products/', views.product_list, name='products'),
   path('products/<int:pk>/', views.product_detail, name='product_detail'),
+  path('products/info/', views.product_info, name='product_info'),
   path('orders/', views.order_list, name='orders'),
 ]
