cd01736a77df2bdba16e5a8ca3c2d7c6dd9e5e38
Winston Trinh <nvth9426@yahoo.com>
2025-12-02 11:23:30 -0500

    Customizing permissions

--- api/views.py
+++ api/views.py
@@ -6,13 +6,19 @@ from api.models import Product, Order
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.views import APIView
 
 class ProductListAPIView(generics.ListCreateAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
 
+  def get_permissions(self):
+    self.permission_classes = [AllowAny]
+    if self.request.method == 'POST':
+      self.permission_classes = [IsAdminUser]
+    return super().get_permissions()
+  
 # class ProductCreateAPIView(generics.CreateAPIView):
 #   model = Product
 #   serializer_class = ProductSerializer
